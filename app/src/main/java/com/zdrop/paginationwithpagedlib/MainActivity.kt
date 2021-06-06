7265e4def748dc61cb8c7cab8cd4c9f3f79dac47
Apurba000Biswas <apurba10024@gmail.com>
2021-06-06 14:38:15 +0600

    Added All Files

---
+++ app/src/main/java/com/zdrop/paginationwithpagedlib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zdrop.paginationwithpagedlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
